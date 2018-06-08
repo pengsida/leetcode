80ce17196ac221d3d60dc3dc5995c46bc3bc80e6
pengsida <291277604@qq.com>
2018-06-08 23:08:27 +0800

    Update leetcode27.cpp

--- leetcode27.cpp
+++ leetcode27.cpp
@@ -13,3 +13,18 @@ public:
         return nums.size();
     }
 };
+
+class Solution2 {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int cur = 0;
+        int size = nums.size();
+        for (int i = 0; i < size; i++) {
+            if (nums[i] != val) {
+                nums[cur] = nums[i];
+                cur++;
+            }
+        }
+        return cur;
+    }
+};
