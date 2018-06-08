5ad3b7e87f9421aa0ea8299f38903aaf21f35876
pengsida <291277604@qq.com>
2018-06-08 20:31:29 +0800

    Update leetcode23.cpp

--- leetcode23.cpp
+++ leetcode23.cpp
@@ -47,3 +47,45 @@ public:
         return cur;
     }
 };
+
+class Solution2 {
+public:
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL)
+            return l2;
+        if (l2 == NULL)
+            return l1;
+        ListNode* head = l1->val <= l2->val ? l1 : l2;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                ListNode* prev = l1;
+                l1 = l1->next;
+                while (l1 && l1->val <= l2->val) {
+                    prev = l1;
+                    l1 = l1->next;
+                }
+                prev->next = l2;
+            } else {
+                ListNode* prev = l2;
+                l2 = l2->next;
+                while (l2 && l2->val < l1->val) {
+                    prev = l2;
+                    l2 = l2->next;
+                }
+                prev->next = l1;
+            }
+        }
+        return head;
+    }
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty())
+            return NULL;
+        while (lists.size() > 1) {
+            lists.push_back(merge(lists[0], lists[1]));
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
+        }
+        return lists.front();
+    }
+};
