0c4875d30cd053aef2bfee6ab0cc2c22f5762c36
pengsida <291277604@qq.com>
2018-06-13 19:39:28 +0800

    Update leetcode30.cpp

--- leetcode30.cpp
+++ leetcode30.cpp
@@ -51,3 +51,72 @@ public:
         return ans;
     }
 };
+
+class Solution2 {
+private:
+    struct Node {
+        Node* child[256] = {0};
+        int num = 0;
+        int rec = 0;
+        Node*& get(char c) {
+            return child[c];
+        }
+    };
+
+private:
+    Node* root;
+    vector<bool> visit;
+    vector<int> ans;
+    vector<Node*> leafs;
+    
+    void insert(Node* cur, string& word, int pos, int od) {
+        if (pos == word.length()) {
+            cur->num++;
+            cur->rec = cur->num;
+            leafs.push_back(cur);
+            return;
+        }
+        auto w = word[pos];
+        auto& it = cur->get(w);
+        if (it == NULL)
+            it = new Node();
+        insert(it, word, pos + 1, od);
+    }
+
+    void find(Node* cur, string& s, int pos, int left, int t_len) {
+        if (cur->num != 0) {
+            if (cur->rec == 0)
+                return;
+            if (left == 1) {
+                ans.push_back(pos - t_len);
+                return;
+            }
+            cur->rec--;
+            find(root, s, pos, left - 1, t_len);
+        }
+        auto w = s[pos];
+        auto it = cur->get(w);
+        if (it == NULL)
+            return;
+        find(it, s, pos + 1, left, t_len);
+    }
+
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        root = new Node();
+        int w_len = words.size();
+        visit = vector<bool>(w_len, false);
+        int t_len = 0;
+        for (int i = 0; i < w_len; i++) {
+            insert(root, words[i], 0, i);
+            t_len += words[i].length();
+        }
+        int s_len = s.length();
+        for (int i = 0; i < s_len; i++) {
+            find(root, s, i, w_len, t_len);
+            for (auto leaf : leafs)
+                leaf->rec = leaf->num;
+        }
+        return ans;
+    }
+};
