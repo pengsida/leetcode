aa403ba73b40592ff4351dcaa6d592714b7148d1
pengsida <291277604@qq.com>
2018-06-14 23:35:30 +0800

    Update leetcode35.cpp

--- leetcode35.cpp
+++ leetcode35.cpp
@@ -9,3 +9,22 @@ public:
         return size;
     }
 };
+
+class Solution2 {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int size = nums.size();
+        int left = 0;
+        int right = size - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (target > nums[mid])
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return right + 1;
+    }
+};
