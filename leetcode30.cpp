e65cbf3437b28de7a77eec926b6df43446066176
pengsida <291277604@qq.com>
2018-06-15 12:01:01 +0800

    Update leetcode30.cpp

--- leetcode30.cpp
+++ leetcode30.cpp
@@ -1,53 +1,52 @@
 class Solution {
 public:
-    vector<int> findSubstring(string S, vector<string> &L) {
+    vector<int> findSubstring(string s, vector<string>& words) {
         vector<int> ans;
-        int n = S.size(), cnt = L.size();
-        if (n <= 0 || cnt <= 0) return ans;
-
-        // init word occurence
-        unordered_map<string, int> dict;
-        for (int i = 0; i < cnt; ++i) dict[L[i]]++;
-
-        // travel all sub string combinations
-        int wl = L[0].size();
-        for (int i = 0; i < wl; ++i) {
-            int left = i, count = 0;
-            unordered_map<string, int> tdict;
-            for (int j = i; j <= n - wl; j += wl) {
-                string str = S.substr(j, wl);
-                // a valid word, accumulate results
-                if (dict.count(str)) {
-                    tdict[str]++;
-                    if (tdict[str] <= dict[str]) 
-                        count++;
-                    else {
-                        // a more word, advance the window left side possiablly
-                        while (tdict[str] > dict[str]) {
-                            string str1 = S.substr(left, wl);
-                            tdict[str1]--;
-                            if (tdict[str1] < dict[str1]) count--;
-                            left += wl;
-                        }
+        if (words.empty())
+            return ans;
+        int w_len = words[0].length();
+        int s_len = s.length();
+        unordered_map<string, int> rec;
+        int target = words.size();
+        for (auto& word : words)
+            rec[word]++;
+        vector<int> rec_num;
+        for (auto it = rec.begin(); it != rec.end(); it++)
+            rec_num.push_back(it->second);
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < w_len; i++) {
+            left = i;
+            right = i;
+            while (right <= s_len - w_len) {
+                auto it = rec.find(s.substr(right, w_len));
+                if (it == rec.end()) {
+                    while (left != right) {
+                        rec.find(s.substr(left, w_len))->second++;
+                        target++;
+                        left += w_len;
                     }
-                    // come to a result
-                    if (count == cnt) {
-                        ans.push_back(left);
-                        // advance one word
-                        tdict[S.substr(left, wl)]--;
-                        count--;
-                        left += wl;
-                    }
+                    right += w_len;
+                    left = right;
+                    continue;
                 }
-                // not a valid word, reset all vars
-                else {
-                    tdict.clear();
-                    count = 0;
-                    left = j + wl;
+                if (it->second == 0) {
+                    rec.find(s.substr(left, w_len))->second++;
+                    target++;
+                    left += w_len;
+                    continue;
                 }
+                it->second--;
+                target--;
+                if (target == 0)
+                    ans.push_back(left);
+                right += w_len;
             }
-        }
-
+            int k = 0;
+            for (auto it = rec.begin(); it != rec.end(); it++)
+                it->second = rec_num[k++];
+            target = words.size();
+        } 
         return ans;
     }
 };
