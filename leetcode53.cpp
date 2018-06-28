ae1f263b67b2e99b9bcc8eede3fae4e48fb2fe7f
pengsida <291277604@qq.com>
2018-06-28 23:10:56 +0800

    Update leetcode53.cpp

--- leetcode53.cpp
+++ leetcode53.cpp
@@ -21,3 +21,35 @@ public:
         return ans;
     }
 };
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void divide_and_comquer(vector<int>& nums, int left, int right, int& mx, int& lmx, int& rmx, int& tol) {
+        if (left == right) {
+            mx = lmx = rmx = tol = nums[left];
+        } else {
+            int mid = (left + right) / 2;
+            int mx1, lmx1, rmx1, tol1;
+            int mx2, lmx2, rmx2, tol2;
+            divide_and_comquer(nums, left, mid, mx1, lmx1, rmx1, tol1);
+            divide_and_comquer(nums, mid + 1, right, mx2, lmx2, rmx2, tol2);
+            tol = tol1 + tol2;
+            mx = max(max(mx1, mx2), rmx1 + lmx2);
+            lmx = max(lmx1, tol1 + lmx2);
+            rmx = max(rmx2, tol2 + rmx1);
+        }
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+        int mx, lmx, rmx, tol;
+        divide_and_comquer(nums, 0, nums.size() - 1, mx, lmx, rmx, tol);
+        return mx;
+    }
+};
